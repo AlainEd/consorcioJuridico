026e03e6be1f5bc1ff1c7d0dddbed90d0a07bc9a
unknown <edson3103a@gmail.com>
2022-04-19 22:16:35 -0400

    Proyecto base

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('ci', 10);
+            $table->string('nombre', 50);
+            $table->string('apellido', 50);
+            $table->enum('sexo', ['M', 'F'])->nullable();
+            $table->date('fecha_nac');
+            $table->string('telefono', 10);
+            $table->boolean('estado');
+            //$table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
